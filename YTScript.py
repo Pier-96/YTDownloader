cd7d48acf8e54ad7d74b7f593f0fa29f7f673c2b
pieroolivares <piero-olivares@hotmail.com>
2022-07-19 21:43:27 +0200

    comments

--- YTScript.py
+++ YTScript.py
@@ -1,7 +1,8 @@
 from ast import arg
 from pytube import YouTube
 
-link = "https://www.youtube.com/watch?v=ZwKvb5ZQjzE"
+# Añade tu link entre los ""
+link = ""
 yt = YouTube(link)
 
 print("Titulo: ", yt.title)
@@ -9,4 +10,5 @@ print("Autor: ", yt.author)
 print("Visitas: ", yt.views)
 
 yd = yt.streams.get_audio_only()
+# Añade tu path donde quieres guardar tus audios
 yd.download('/mnt/c/Users/PieroOP/OneDrive/Escritorio/audios')
